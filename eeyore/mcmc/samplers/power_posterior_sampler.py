5ebf9bb2b54cba0ad31a40f8de1813bb5b45ce8f
Theodore Papamarkou <papamarkout@ornl.gov>
2019-09-12 23:44:47 -0400

    Added methods for picking neighbouring chains in power posterior sampler

--- eeyore/mcmc/samplers/power_posterior_sampler.py
+++ eeyore/mcmc/samplers/power_posterior_sampler.py
@@ -1,4 +1,9 @@
+from itertools import chain
+
+import numpy as np
+
 import torch
+from torch.distributions import Categorical
 
 from eeyore.api import Sampler
 from eeyore.mcmc import MCChain
@@ -6,8 +11,9 @@ from .metropolis_hastings import MetropolisHastings
 from .mala import MALA
 
 class PowerPosteriorSampler(Sampler):
-    def __init__(self, model, theta0, dataloader, samplers, temperatures=None):
+    def __init__(self, model, theta0, dataloader, samplers, temperatures=None, b=0.5):
         super(PowerPosteriorSampler, self).__init__()
+        self.b = b
 
         self.num_chains = len(samplers)
 
@@ -32,9 +38,25 @@ class PowerPosteriorSampler(Sampler):
             else:
                 ValueError
 
+        self.categoricals = []
+        for i in range(self.num_chains):
+            self.categoricals.append(Categorical(self.categorical_probabilities(i)))
+
         self.chains = []
         for i in range(self.num_chains):
             self.chains.append(MCChain(self.samplers[i].keys))
+
+    def categorical_probability(self, j, i):
+        eb = np.exp(-self.b)
+        numerator = eb**np.absolute(j-i)
+        denominator = eb*(2-eb**i-eb**(self.num_chains-1-i))/(1-eb)
+        return numerator/denominator
+
+    def categorical_probabilities(self, i):
+        return torch.tensor([self.categorical_probability(j, i) for j in chain(range(i), range(i+1, self.num_chains))])
+
+    def sample_categorical(self, i):
+        return list(chain(range(i), range(i+1, 11)))[self.categoricals[i].sample().item()]
 
     def reset(self, theta):
         for sampler in self.samplers:
