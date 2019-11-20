8f97a69b777e902ed1d336f0f4da95c2e0a45e6f
Theodore Papamarkou <papamarkout@ornl.gov>
2019-11-20 00:49:59 -0500

    Edits in DEMC

--- eeyore/mcmc/samplers/demc.py
+++ eeyore/mcmc/samplers/demc.py
@@ -13,18 +13,16 @@ from eeyore.mcmc import ChainFile, ChainList
 from .metropolis_hastings import MetropolisHastings
 
 class DEMC(Sampler):
-    def __init__(self, model, theta0, dataloader, sigma, num_chains=10, c=0.1, schedule=lambda n, num_iterations: 1.,
+    def __init__(self, models, theta0s, dataloaders, sigmas, num_chains=10, c=0.1, schedule=lambda n, num_iterations: 1.,
     storage='list', keys=['theta', 'target_val', 'accepted'], path=os.getcwd(), mode='a'):
         super(DEMC, self).__init__()
+        self.models = models
+        self.dataloaders = dataloaders
         self.num_chains = num_chains
-        self.sigma = sigma
+        self.sigmas = sigmas
         self.c = c
         self.schedule = schedule
         self.temperature = None
-
-        self.models = []
-        for i in range(self.num_chains):
-            self.models.append(copy.deepcopy(model))
 
         self.chains = []
         for i in range(self.num_chains):
@@ -39,8 +37,8 @@ class DEMC(Sampler):
         self.samplers = []
         for i in range(self.num_chains):
             self.samplers.append(MetropolisHastings(
-                self.models[i], theta0, dataloader, symmetric=True,
-                kernel=DEMCKernel(self.sigma, c=0.1, dtype=self.models[i].dtype, device=self.models[i].device),
+                self.models[i], theta0s[i], self.dataloaders[i], symmetric=True,
+                kernel=DEMCKernel(self.sigmas[i], c=0.1, dtype=self.models[i].dtype, device=self.models[i].device),
                 chain=self.chains[i]
             ))
 
