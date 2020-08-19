Fixes in inse mc covariance

eeyore/linalg/is_pos_def.py
@@ -1,7 +1,7 @@
 import torch
 
 def is_pos_def(x):
-    if torch.equal(x, torch.t(x)):
+    if torch.equal(x, x.t()):
         try:
             torch.cholesky(x)
             return True

eeyore/stats/inse_mc_cov.py
@@ -7,7 +7,7 @@ import torch
 from eeyore.linalg import is_pos_def
 
 def inse_mc_cov(x, adjust=False):
-    x = x - x.mean(0)
+    x_ctr = x - x.mean(0)
 
     n, p = x.shape
 
@@ -22,15 +22,15 @@ def inse_mc_cov(x, adjust=False):
         gam1 = torch.zeros([p, p], dtype=x.dtype)
 
         for i in range(n - 2 * m):
-            gam0 = gam0 + torch.ger(x[i, :], x[i + 2 * m, :])
+            gam0 = gam0 + torch.ger(x_ctr[i, :], x_ctr[i + 2 * m, :])
         gam0 = gam0 / n
 
         for i in range(n - 2 * m - 1):
-            gam1 = gam1 + torch.ger(x[i, :], x[i + 2 * m + 1, :])
+            gam1 = gam1 + torch.ger(x_ctr[i, :], x_ctr[i + 2 * m + 1, :])
         gam1 = gam1 / n
 
         Gam = gam0 + gam1
-        Gam = (Gam + torch.t(Gam)) / 2
+        Gam = (Gam + Gam.t()) / 2
 
         if m == 0:
             Sig = -gam0 + 2 * Gam
@@ -51,15 +51,15 @@ def inse_mc_cov(x, adjust=False):
         gam1 = torch.zeros([p, p], dtype=x.dtype)
 
         for i in range(n - 2 * m):
-            gam0 = gam0 + torch.ger(x[i, :], x[i + 2 * m, :])
+            gam0 = gam0 + torch.ger(x_ctr[i, :], x_ctr[i + 2 * m, :])
         gam0 = gam0 / n
 
         for i in range(n - 2 * m - 1):
-            gam1 = gam1 + torch.ger(x[i, :], x[i + 2 * m + 1, :])
+            gam1 = gam1 + torch.ger(x_ctr[i, :], x_ctr[i + 2 * m + 1, :])
         gam1 = gam1 / n
 
         Gam = gam0 + gam1
-        Gam = (Gam + torch.t(Gam)) / 2
+        Gam = (Gam + Gam.t()) / 2
 
         Sig1 = Sig + 2 * Gam
 
@@ -75,7 +75,7 @@ def inse_mc_cov(x, adjust=False):
         if adjust:
             eigenvals, eigenvecs = torch.symeig(Gam, eigenvectors=True)
             eigenvals[eigenvals > 0] = 0
-            Gamadj = Gamadj - eigenvecs @ torch.diag(eigenvals) @ torch.t(eigenvecs)
+            Gamadj = Gamadj - eigenvecs @ torch.diag(eigenvals) @ eigenvecs.t()
 
     if adjust:
         Sig = Sig + 2 * Gamadj