a0702237e80b9698d5347b17d0c02f714db82a15
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2020-08-31 21:10:38 -0400

    Updated xor mlp example

--- examples/mlp/xor/metropolis_hastings.py
+++ examples/mlp/xor/metropolis_hastings.py
@@ -2,11 +2,8 @@
 #
 # Learn the XOR function by sampling the weights of a multi-layer perceptron (MLP) via the Metropolis-Hastings algorithm.
 
-# %% Import packages
+# %% Import packages for MCMC simulation
 
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
 import torch
 
 from torch.distributions import Normal
@@ -29,15 +26,14 @@ hparams = mlp.Hyperparameters(dims=[2, 2, 1])
 model = mlp.MLP(loss=loss_functions['binary_classification'], hparams=hparams, dtype=torch.float64)
 model.prior = Normal(
     torch.zeros(model.num_params(), dtype=model.dtype),
-    np.sqrt(3)*torch.ones(model.num_params(), dtype=model.dtype)
+    (3 * torch.ones(model.num_params(), dtype=model.dtype)).sqrt()
 )
 
 # %% Setup Metropolis-Hastings sampler
 
-proposal_scale = 1.55
 kernel = NormalKernel(
     torch.zeros(model.num_params(), dtype=model.dtype),
-    torch.sqrt(torch.tensor(proposal_scale, dtype=model.dtype)) * torch.ones(model.num_params(), dtype=model.dtype)
+    (1.55 * torch.ones(model.num_params(), dtype=model.dtype)).sqrt()
 )
 sampler = MetropolisHastings(model, theta0=model.prior.sample(), dataloader=dataloader, kernel=kernel)
 
@@ -45,44 +41,58 @@ sampler = MetropolisHastings(model, theta0=model.prior.sample(), dataloader=data
 
 sampler.run(num_epochs=11000, num_burnin_epochs=1000, verbose=True, verbose_step=1000)
 
+# %% Import kanga package for visual MCMC summaries
+
+import kanga.plots as ps
+
+# %% Generate kanga ChainArray from eeyore ChainList
+
+chain_array = sampler.get_chain().to_kanga()
+
 # %% Compute acceptance rate
 
-print('Acceptance rate: {}'.format(sampler.get_chain().acceptance_rate()))
+print('Acceptance rate: {}'.format(chain_array.acceptance_rate()))
 
 # %% Compute Monte Carlo mean
 
-print('Monte Carlo mean: {}'.format(sampler.get_chain().mean()))
+print('Monte Carlo mean: {}'.format(chain_array.mean()))
+
+# %% Compute Monte Carlo standard error
+
+print('Monte Carlo standard error: {}'.format(chain_array.mc_se()))
+
+# %% Compute multivariate ESS
+
+print('Multivariate ESS: {}'.format(chain_array.multi_ess()))
 
 # %% Plot traces of simulated Markov chain
 
 for i in range(model.num_params()):
-    chain = sampler.get_sample(i)
-    plt.figure()
-    sns.lineplot(range(len(chain)), chain)
-    plt.xlabel('Iteration')
-    plt.ylabel('Parameter value')
-    plt.title(r'Traceplot of $\theta_{{{0}}}$'.format(i+1))
+    ps.trace(
+        chain_array.get_param(i),
+        title=r'Traceplot of $\theta_{{{}}}$'.format(i+1),
+        xlabel='Iteration',
+        ylabel='Parameter value'
+    )
 
 # %% Plot running means of simulated Markov chain
 
 for i in range(model.num_params()):
-    chain = sampler.get_sample(i)
-    chain_mean = torch.empty(len(chain))
-    chain_mean[0] = chain[0]
-    for j in range(1, len(chain)):
-        chain_mean[j] = (chain[j]+j*chain_mean[j-1])/(j+1)
-
-    plt.figure()
-    sns.lineplot(range(len(chain)), chain_mean)
-    plt.xlabel('Iteration')
-    plt.ylabel('Parameter value')
-    plt.title(r'Running mean of $\theta_{{{0}}}$'.format(i+1))
+    ps.running_mean(
+        chain_array.get_param(i),
+        title=r'Running mean plot of parameter $\theta_{{{}}}$'.format(i+1),
+        xlabel='Iteration',
+        ylabel='Running mean'
+    )
 
 # %% Plot histograms of marginals of simulated Markov chain
 
-for i in range(model.num_params()):
-    plt.figure()
-    sns.distplot(sampler.get_sample(i), bins=20, norm_hist=True)
-    plt.xlabel('Value range')
-    plt.ylabel('Relative frequency')
-    plt.title(r'Histogram of $\theta_{{{0}}}$'.format(i+1))
+for i in range(model.num_params()):    
+    ps.hist(
+        chain_array.get_param(i),
+        bins=30,
+        density=True,
+        title=r'Histogram of parameter $\theta_{{{}}}$'.format(i+1),
+        xlabel='Parameter value',
+        ylabel='Parameter relative frequency'
+    )
